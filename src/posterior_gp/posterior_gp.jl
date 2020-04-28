7bc915c00f457ea742f299f33f8e9f763499177a
willtebbutt <wt0881@my.bristol.ac.uk>
2020-04-28 12:02:02 +0100

    Tweak posterior docstring

--- src/posterior_gp/posterior_gp.jl
+++ src/posterior_gp/posterior_gp.jl
@@ -8,9 +8,15 @@ end
 
 Constructs the posterior distribution over `fx.f` given observations `y` at `x` made under
 noise `fx.Σ`. This is another `AbstractGP` object. See chapter 2 of [1] for a recap on
-exact inference in GPs.
-
-
+exact inference in GPs. This posterior process has mean function
+```julia
+m_posterior(x) = m(x) + k(x, fx.x) inv(cov(fx)) (y - mean(fx))
+```
+and kernel
+```julia
+k_posterior(x, z) = k(x, z) - k(x, fx.x) inv(cov(fx)) k(fx.x, z)
+```
+where `m` and `k` are the mean function and kernel of `fx.f` respectively.
 """
 function posterior(fx::FiniteGP, y::AbstractVector{<:Real})
     m, C_mat = mean_and_cov(fx)
