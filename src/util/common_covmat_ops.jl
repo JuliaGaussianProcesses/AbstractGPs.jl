595c6b71b98d6be17b9aa9fd95cc3eb29e3b5f5e
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-05-12 17:05:45 +0530

    Sequential Conditioning on exact posterior (#7)
    * Add sequential update for exact posterior
    
    * Add TODOs
    
    * Rename variables in update_chol and add tests for the same
    
    * Rename variables in posterior
    
    * Clean up
    
    * Fix tests
    
    * Fix tests and clean up
    
    * Organise tests
    
    * Fix tests
    
    * Address code review suggestions.
    
    * Move update_chol to common_covmat_ops.jl
    
    * Update posterior docstring
    
    * Update src/posterior_gp/posterior_gp.jl
    
    Co-authored-by: willtebbutt <wt0881@my.bristol.ac.uk>
    
    * Bump version to 0.1.1
    
    Co-authored-by: willtebbutt <wt0881@my.bristol.ac.uk>

--- src/util/common_covmat_ops.jl
+++ src/util/common_covmat_ops.jl
@@ -1,3 +1,31 @@
+
+"""
+     update_chol(chol::Cholesky, C12::AbstractMatrix, C22::AbstractMatrix)
+
+Let `C` be the positive definite matrix comprising blocks
+```julia
+C = [C11 C12;
+     C21 C22]
+```
+with upper-triangular cholesky factorisation comprising blocks
+```julia
+U = [U11 U12;
+     0   U22]
+```
+where `U11` and `U22` are themselves upper-triangular, and `U11 = cholesky(C11).U`.
+update_chol computes the updated Cholesky given original `chol`, `C12`, and `C22`.
+
+# Arguments
+     - chol::Cholesky: The original cholesky decomposition
+     - C12::AbstractMatrix: matrix of size (size(chol.U, 1), size(C22, 1))
+     - C22::AbstractMatrix: positive-definite matrix
+"""
+function update_chol(chol::Cholesky, C12::AbstractMatrix, C22::AbstractMatrix)
+    U12 = chol.U' \ C12
+    U22 = cholesky(Symmetric(C22 - U12'U12)).U
+    return Cholesky([chol.U U12; zero(U12') U22], 'U', 0)
+end
+
 # Various specialised operations using the Cholesky factorisation.
 
 Xt_A_X(A::Cholesky, x::AbstractVector) = sum(abs2, A.U * x)
