a5390ad8b268bc964c0dc0895ebcd6971c05fb0a
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-05-14 20:14:16 +0530

    Fix tests

--- test/util/plotting.jl
+++ test/util/plotting.jl
@@ -2,7 +2,7 @@
     
     x = rand(10)
     gp = GP(SqExponentialKernel())(x)
-    plt1 = sampleplot(gp, samples=10)
+    plt1 = sampleplot(gp, 10)
     @test plt1.n == 10
 
     rec = RecipesBase.apply_recipe(Dict{Symbol, Any}(), gp)
