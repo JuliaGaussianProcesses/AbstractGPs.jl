7b0621b95d0fe8af054936a4ce2ae51b74702a36
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-05-30 18:27:22 +0530

    Set random seed for tests

--- test/posterior_gp/approx_posterior_gp.jl
+++ test/posterior_gp/approx_posterior_gp.jl
@@ -29,12 +29,12 @@
     a = collect(range(-1.0, 1.0; length=N_a))
     b = randn(rng, N_b)
     abstractgp_interface_tests(f_approx_post, a, b)
-
+    
     @testset "update_approx_posterior (new observation)" begin
-
-        X = rand(10)
-        y = rand(10)
-        Z = rand(4)
+        rng = MersenneTwister(1)
+        X = rand(rng, 10)
+        y = rand(rng, 10)
+        Z = rand(rng, 4)
 
         f = GP(SqExponentialKernel())
 
@@ -55,14 +55,15 @@
         @test u_p_fx1.data.B_εf ≈ p_fx2.data.B_εf atol=1e-5
         @test u_p_fx1.data.x ≈ p_fx2.data.x atol=1e-5
         @test u_p_fx1.data.Σy ≈ p_fx2.data.Σy atol=1e-5
-        
+
     end
 
     @testset "update_approx_posterior (new pseudo-points)" begin
-        X = rand(10)
-        y = rand(10)
-        Z1 = rand(4)
-        Z2 = rand(3)
+        rng = MersenneTwister(1)
+        X = rand(rng, 10)
+        y = rand(rng, 10)
+        Z1 = rand(rng, 4)
+        Z2 = rand(rng, 3)
         Z = vcat(Z1, Z2)
 
         f = GP(SqExponentialKernel())
@@ -77,7 +78,7 @@
         @test u_p_fx1.data.m_ε ≈ p_fx2.data.m_ε atol=1e-5
         @test u_p_fx1.data.Λ_ε.U ≈ p_fx2.data.Λ_ε.U atol=1e-5
         @test u_p_fx1.data.U ≈ p_fx2.data.U atol=1e-5
-        @test u_p_fx1.data.α ≈ p_fx2.data.α atol=1e-1
+        @test u_p_fx1.data.α ≈ p_fx2.data.α atol=1e-2
         @test u_p_fx1.data.z ≈ p_fx2.data.z atol=1e-5
         @test u_p_fx1.data.b_y ≈ p_fx2.data.b_y atol=1e-5
         @test u_p_fx1.data.B_εf ≈ p_fx2.data.B_εf atol=1e-5
