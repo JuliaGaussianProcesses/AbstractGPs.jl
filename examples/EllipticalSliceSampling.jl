c63d3107c063975d749aaf43dd8d00db48a01092
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-05-31 13:47:23 +0530

    Address code review

--- examples/EllipticalSliceSampling.jl
+++ examples/EllipticalSliceSampling.jl
@@ -17,14 +17,14 @@
 
 # Loading the necessary packages.
 
-using AbstractGPs, KernelFunctions, Plots, CSV, DataFrames
+using AbstractGPs, KernelFunctions, Plots
 include("utils.jl")
 
 # Loading [toy regression dataset](https://github.com/GPflow/docs/blob/master/doc/source/notebooks/basics/data/regression_1D.csv) taken from GPFlow examples.
 
-df = CSV.read("data/regression_1D.csv", header=false);
-x = df[:, 1];
-y = df[:, 2];
+x = [0.8658165855998895, 0.6661700880180962, 0.8049218148148531, 0.7714303440386239, 0.14790478354654835, 0.8666105548197428, 0.007044577166530286, 0.026331737288148638, 0.17188596617099916, 0.8897812990554013, 0.24323574561119998, 0.028590102134105955];
+y = [1.5255314337144372, 3.6434202968230003, 3.010885733911661, 3.774442382979625, 3.3687639483798324, 1.5506452040608503, 3.790447985799683, 3.8689707574953, 3.4933565751758713, 1.4284538820635841, 3.8715350915692364, 3.7045949061144983];
+scatter(x, y, xlabel="x", ylabel="y")
 
 # Making a custom kernel with two parameters.
 
@@ -76,7 +76,7 @@ end
 
 # We define a Gaussian prior over the joint distribution on kernel parameters space. Since we have only two parameters, we define a multi-variate Gaussian of dimension two.
 
-prior = MvNormal(zeros(2), ones(2) .* 1)
+prior = MvNormal(2, 1)
 
 # Sanity check for the defined `logp` function and `prior` distribution.
 
