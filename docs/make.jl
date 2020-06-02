6c4b040650ca2b9eba2a351b44d1ebc24489e4ed
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-06-03 01:33:06 +0530

    Setup Literate.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,19 @@
-using Documenter, AbstractGPs
+using Documenter, AbstractGPs, KernelFunctions
+
+using Literate
+using Plots # to not capture precompilation output
+
+Literate.markdown(joinpath(@__DIR__, "..", "examples/EllipticalSliceSampling.jl"), joinpath(@__DIR__, "src/generated"); name = "EllipticalSliceSampling")
 
 makedocs(;
     modules=[AbstractGPs],
     format=Documenter.HTML(),
-    pages=[
-        "Home" => "index.md",
-    ],
     repo="https://github.com/JuliaGaussianProcesses/AbstractGPs.jl/blob/{commit}{path}#L{line}",
     sitename="AbstractGPs.jl",
+    pages = Any[
+        "index.md",
+        "generated/EllipticalSliceSampling.md"
+    ],
     authors="willtebbutt <wt0881@my.bristol.ac.uk>",
     assets=String[],
 )
