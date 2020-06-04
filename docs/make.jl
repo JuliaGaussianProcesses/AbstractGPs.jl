ce891f9f8b47b63b9113f4bcd3ed9c0385152c9d
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-06-04 20:57:04 +0530

    Preprocess example scripts

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,11 @@ using Documenter, AbstractGPs
 using Literate
 using Plots # to not capture precompilation output
 
-Literate.markdown(joinpath(@__DIR__, "..", "examples/EllipticalSliceSampling.jl"), joinpath(@__DIR__, "src/generated"); name = "EllipticalSliceSampling")
+function preprocessing(str)
+    return replace(str, r"# ---[a-zA-Z0-9 #:,.'-_\n]*# ---\n" => ""; count=1)
+end
+
+Literate.markdown(joinpath(@__DIR__, "..", "examples/EllipticalSliceSampling.jl"), joinpath(@__DIR__, "src/generated"); name = "EllipticalSliceSampling", preprocess = preprocess)
 
 DocMeta.setdocmeta!(
     AbstractGPs,
