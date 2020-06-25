a2c7e95b9bd136e689aad65192982f0d29b9ed21
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-06-25 14:28:46 +0530

    Generate all examples

--- docs/make.jl
+++ docs/make.jl
@@ -3,12 +3,22 @@ using Documenter, AbstractGPs
 using Literate
 using Plots # to not capture precompilation output
 
-Literate.markdown(
-    joinpath(@__DIR__, "..", 
-    "examples/EllipticalSliceSampling.jl"), 
-    joinpath(@__DIR__, "src/generated"); 
-    name = "EllipticalSliceSampling", 
-)
+example_files = filter(x->occursin(r".jl$",x), readdir(joinpath(@__DIR__, "..", "examples")))
+generated_examples = Array{String}(undef, length(example_files))
+
+println(example_files)
+for (i, example) in enumerate(example_files)
+    println(joinpath(@__DIR__, "..", "examples", example))
+    Literate.markdown(
+        joinpath(@__DIR__, "..", "examples", example),
+        joinpath(@__DIR__, "src/generated");
+        name = example[begin:end-3],
+    )
+    generated_examples[i] = joinpath("generated", string(split(example, ".jl")[1], ".md"))
+end
+
+
+
 
 DocMeta.setdocmeta!(
     AbstractGPs,
@@ -24,7 +34,7 @@ makedocs(;
     sitename="AbstractGPs.jl",
     pages = Any[
         "index.md",
-        "generated/EllipticalSliceSampling.md"
+        generated_examples...
     ],
     authors="willtebbutt <wt0881@my.bristol.ac.uk>",
     assets=String[],
@@ -32,4 +42,5 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/JuliaGaussianProcesses/AbstractGPs.jl",
+    push_preview=true
 )
