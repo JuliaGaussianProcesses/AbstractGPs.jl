16b84cc52a2457f745839f9c1cae3cc1002106a0
willtebbutt <wct23@cam.ac.uk>
2020-07-20 17:32:20 +0100

    Resolves #14 (#16)
    * Fix bug
    
    * Adds bounds for ChainRulesCore
    
    * Exports ColVecs and RowVecs
    
    * Update bounds
    
    * Export ColVecs / RowVecs
    
    * Bump patch version

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,20 @@ using AbstractGPs: AbstractGP, MeanFunction, FiniteGP, ConstMean, GP, ZeroMean,
     ConstMean, CustomMean, Xt_A_X, Xt_A_Y, Xt_invA_Y, Xt_invA_X, diag_At_A, diag_At_B, 
     diag_Xt_A_X, diag_Xt_A_Y, diag_Xt_invA_X, diag_Xt_invA_Y, Xtinv_A_Xinv, tr_At_A,
     mean_and_cov_diag
+
 using Documenter
+using ChainRulesCore
 using Distributions: MvNormal, PDMat
+using FiniteDifferences
+using FiniteDifferences: j′vp, to_vec
 using KernelFunctions
 using KernelFunctions: Kernel, ColVecs, RowVecs
 using LinearAlgebra
 using LinearAlgebra: AbstractTriangular
-using Random
 using Plots
-using Test
-using FiniteDifferences
-using FiniteDifferences: j′vp, to_vec
+using Random
 using Statistics
+using Test
 using Zygote
 
 
