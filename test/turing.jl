1002fed629d5a78264e6f833c61abedcfc9b6f6b
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-09-05 17:44:12 +0200

    Modified syntax Turing test and changed test_nowarn to test

--- test/turing.jl
+++ test/turing.jl
@@ -3,14 +3,14 @@
     y = randn(3)
     X = randn(3, 1)
     x = [rand(1) for _ in 1:3]
-    @model GPRegression(y, X) = begin
+    @model function GPRegression(y, X)
         # Priors.
         α ~ LogNormal(0.0, 0.1)
         ρ ~ LogNormal(0.0, 1.0)
         σ² ~ LogNormal(0.0, 1.0)
 
         # Realized covariance function
-        kernel = α * transform(SqExponentialKernel(), 1/ρ)
+        kernel = α * transform(SqExponentialKernel(), 1 / ρ)
         f = GP(kernel)
 
         # Sampling Distribution.
@@ -18,8 +18,8 @@
     end
     # Test for matrices
     m = GPRegression(y, RowVecs(X))
-    @test_nowarn sample(m, HMC(0.5, 1), 5)
+    @test length(sample(m, HMC(0.5, 1), 5)) == 5
     # Test for vectors of vector
     m = GPRegression(y, x)
-    @test_nowarn sample(m, HMC(0.5, 1), 5)
+    @test length(sample(m, HMC(0.5, 1), 5)) == 5
 end
