f15a82fdc60c1814727c3a1693c9aceb149df93f
4aHxKzD <57544666+4aHxKzD@users.noreply.github.com>
2020-09-10 18:13:18 +0100

    Corrects what appears to be an error in a docstring.

--- src/util/plotting.jl
+++ src/util/plotting.jl
@@ -28,7 +28,7 @@ function.
 ```julia
 using Plots
 f = GP(SqExponentialKernel())
-sampleplot(f(rand(10), 10; markersize=5)
+sampleplot(f(rand(10)), 10; markersize=5)
 ```
 The given example plots 10 samples from the given `FiniteGP`. The `markersize` is modified
 from default of 0.5 to 5.
