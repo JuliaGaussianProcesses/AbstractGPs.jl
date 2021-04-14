0a0176325055dcc2aa30c171f1a56c677640070d
David Widmann <dev+github@devmotion.de>
2021-04-14 23:04:29 +0200

    Introduce test groups

--- test/runtests.jl
+++ test/runtests.jl
@@ -36,56 +36,60 @@ using Statistics
 using Test
 using Zygote
 
+const GROUP = get(env, "GROUP", "All")
+
 include("test_util.jl")
 
 @testset "AbstractGPs" begin
-    # @testset "util" begin
-    #     include(joinpath("util", "common_covmat_ops.jl"))
-    #     include(joinpath("util", "plotting.jl"))
-    # end
-    # println(" ")
-    # @info "Ran util tests"
+    if GROUP == "All" || GROUP == "AbstractGP"
+        @testset "util" begin
+            include(joinpath("util", "common_covmat_ops.jl"))
+            include(joinpath("util", "plotting.jl"))
+        end
+        println(" ")
+        @info "Ran util tests"
 
-    # @testset "abstract_gp" begin
-    #     include(joinpath("abstract_gp", "abstract_gp.jl"))
-    #     include(joinpath("abstract_gp", "finite_gp.jl"))
-    # end
-    # println(" ")
-    # @info "Ran abstract_gp tests"
+        @testset "abstract_gp" begin
+            include(joinpath("abstract_gp", "abstract_gp.jl"))
+            include(joinpath("abstract_gp", "finite_gp.jl"))
+        end
+        println(" ")
+        @info "Ran abstract_gp tests"
 
-    # @testset "gp" begin
-    #     include(joinpath("gp", "mean_functions.jl"))
-    #     include(joinpath("gp", "gp.jl"))
-    # end
-    # println(" ")
-    # @info "Ran gp tests"
+        @testset "gp" begin
+            include(joinpath("gp", "mean_functions.jl"))
+            include(joinpath("gp", "gp.jl"))
+        end
+        println(" ")
+        @info "Ran gp tests"
 
-    # @testset "posterior_gp" begin
-    #     include(joinpath("posterior_gp", "posterior_gp.jl"))
-    #     include(joinpath("posterior_gp", "approx_posterior_gp.jl"))
-    # end
-    # println(" ")
-    # @info "Ran posterior_gp tests"
+        @testset "posterior_gp" begin
+            include(joinpath("posterior_gp", "posterior_gp.jl"))
+            include(joinpath("posterior_gp", "approx_posterior_gp.jl"))
+        end
+        println(" ")
+        @info "Ran posterior_gp tests"
 
-    # include(joinpath("latent_gp", "latent_gp.jl"))
-    # println(" ")
-    # @info "Ran latent_gp tests"
+        include(joinpath("latent_gp", "latent_gp.jl"))
+        println(" ")
+        @info "Ran latent_gp tests"
 
-    # include("deprecations.jl")
-    # println(" ")
-    # @info "Ran deprecation tests"
+        include("deprecations.jl")
+        println(" ")
+        @info "Ran deprecation tests"
 
-    # @testset "doctests" begin
-    #     DocMeta.setdocmeta!(
-    #         AbstractGPs,
-    #         :DocTestSetup,
-    #         :(using AbstractGPs, Random, Documenter, LinearAlgebra);
-    #         recursive=true,
-    #     )
-    #     doctest(AbstractGPs)
-    # end
+        @testset "doctests" begin
+            DocMeta.setdocmeta!(
+                AbstractGPs,
+                :DocTestSetup,
+                :(using AbstractGPs, Random, Documenter, LinearAlgebra);
+                recursive=true,
+            )
+            doctest(AbstractGPs)
+        end
+    end
 
-    @static if VERSION >= v"1.5"
+    if (GROUP == "All" || GROUP == "Compat") && VERSION >= v"1.5"
         Pkg.activate("compat")
         Pkg.develop(PackageSpec(; path=".."))
         Pkg.instantiate()
