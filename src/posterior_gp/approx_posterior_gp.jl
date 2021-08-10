74f127a4539d3f0277c89a60692049e497dc7f39
Ross Viljoen <ross@viljoen.co.uk>
2021-08-10 11:53:09 +0100

    Explain jitter in docstring

--- src/posterior_gp/approx_posterior_gp.jl
+++ src/posterior_gp/approx_posterior_gp.jl
@@ -3,7 +3,9 @@
 
 The "Variational Free Energy" sparse approximation [1], used to construct an
 approximate posterior with inducing inputs `z`. See [`posterior(v::VFE,
-fx::FiniteGP, y::AbstractVector{<:Real})`](@ref) for a usage example.
+fx::FiniteGP, y::AbstractVector{<:Real})`](@ref) for a usage example. The
+`jitter` term is added to the diagonal of the covariance matrix of the inducing
+points to aid numerical stability.
 
 [1] - M. K. Titsias. "Variational learning of inducing variables in sparse Gaussian
 processes". In: Proceedings of the Twelfth International Conference on Artificial
