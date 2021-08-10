f2c1592c0d670484c2bf61a9038b330e8c94646c
Ross Viljoen <ross@viljoen.co.uk>
2021-08-10 12:47:39 +0100

    Update regression example

--- examples/regression-1d/script.jl
+++ examples/regression-1d/script.jl
@@ -368,7 +368,7 @@ plt
 # Sanity check for the Evidence Lower BOund (ELBO) implemented according to
 # M. K. Titsias's _Variational learning of inducing variables in sparse Gaussian processes_.
 
-elbo(fx, y_train, f(rand(5)))
+elbo(VFE(rand(5)), fx, y_train)
 
 # We use the LBFGS algorithm to maximize the given ELBO. It is provided by the Julia
 # package [Optim.jl](https://github.com/JuliaNLSolvers/Optim.jl).
@@ -393,8 +393,8 @@ function objective_function(x, y)
         f = GP(kernel)
         fx = f(x, 0.1)
         z = logistic.(params[3:end])
-        fz = f(z, 1e-6)  # "observing" the latent process with some (small) amount of jitter improves numerical stability
-        return -elbo(fx, y, fz)
+        approx = VFE(z; jitter=1e-6)
+        return -elbo(approx, fx, y)
     end
     return negative_elbo
 end
@@ -427,7 +427,7 @@ opt_kernel =
     (Matern52Kernel() ∘ ScaleTransform(softplus(opt.minimizer[2])))
 opt_f = GP(opt_kernel)
 opt_fx = opt_f(x_train, 0.1)
-ap = approx_posterior(VFE(), opt_fx, y_train, opt_f(logistic.(opt.minimizer[3:end])))
+ap = posterior(VFE(logistic.(opt.minimizer[3:end])), opt_fx, y_train)
 logpdf(ap(x_test), y_test)
 
 # We visualize the approximate posterior with optimized parameters.
