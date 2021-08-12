b649546e03bde843a7618df6550c521ba2158096
ST John <st--@users.noreply.github.com>
2021-08-12 23:01:24 +0300

    clean up descriptions

--- src/AbstractGPs.jl
+++ src/AbstractGPs.jl
@@ -37,21 +37,23 @@ export GP,
 # Various bits of utility functionality.
 include(joinpath("util", "common_covmat_ops.jl"))
 
-# AbstractGP interface and FiniteGP interface.
+# AbstractGP interface.
 include("abstract_gp.jl")
+
+# FiniteGP object that describes the projection of a GP at points x.
 include("finite_gp_projection.jl")
 
-# Basic GP object.
+# Basic GP object, e.g. to define the prior.
 include("mean_function.jl")
 include("base_gp.jl")
 
-# Efficient exact posterior GP implementation
+# Efficient exact posterior GP implementation.
 include("exact_gpr_posterior.jl")
 
-# Approximate GP inference
+# Approximate sparse GP inference for Gaussian likelihood.
 include("sparse_approximations.jl")
 
-# LatentGP object to accommodate GPs with non-Gaussian likelihoods.
+# LatentGP and LatentFiniteGP objects to accommodate GPs with non-Gaussian likelihoods.
 include("latent_gp.jl")
 
 # Plotting utilities.
@@ -62,4 +64,5 @@ include(joinpath("util", "test_util.jl"))
 
 # Deprecations.
 include("deprecations.jl")
+
 end # module
