a31770829c6856f1645b240428a0ef983528576d
ST John <st--@users.noreply.github.com>
2021-11-02 15:11:21 +0200

    fixes

--- src/latent_gp.jl
+++ src/latent_gp.jl
@@ -1,5 +1,5 @@
 """
-    LatentGP(f<:GP, lik, Σy)
+    LatentGP(f<:AbstractGP, lik, Σy)
 
  - `f` is a `AbstractGP`.
  - `lik` is the likelihood function which maps samples from `f` to the
@@ -7,7 +7,6 @@
    `Distribution` compatible with the observations).
  - `Σy` is the noise under which the latent GP is "observed"; this represents
    the jitter used to avoid numeric instability and should generally be small.
-    
 """
 struct LatentGP{Tf<:AbstractGP,Tlik,TΣy}
     f::Tf
@@ -22,7 +21,6 @@ end
  - `lik` is the likelihood function which maps samples from `f` to the
    corresponding conditional likelihood distributions (i.e., `lik` must return a
    `Distribution` compatible with the observations).
-    
 """
 struct LatentFiniteGP{Tfx<:FiniteGP,Tlik}
     fx::Tfx
