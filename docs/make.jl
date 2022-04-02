f84ef8311124f3833068d592b05fae8d9b6eee93
ST John <st--@users.noreply.github.com>
2022-04-02 17:09:38 +0300

    use JuliaGPsDocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,47 +1,17 @@
 ### Process examples
-# Always rerun examples
-const EXAMPLES_OUT = joinpath(@__DIR__, "src", "examples")
-ispath(EXAMPLES_OUT) && rm(EXAMPLES_OUT; recursive=true)
-mkpath(EXAMPLES_OUT)
+using Pkg
+Pkg.add(Pkg.PackageSpec(; url="https://github.com/JuliaGaussianProcesses/JuliaGPsDocs.jl")) # While the package is unregistered, it's a workaround
 
-# Install and precompile all packages
-# Workaround for https://github.com/JuliaLang/Pkg.jl/issues/2219
-examples = filter!(isdir, readdir(joinpath(@__DIR__, "..", "examples"); join=true))
-let script = "using Pkg; Pkg.activate(ARGS[1]); Pkg.instantiate()"
-    for example in examples
-        if !success(`$(Base.julia_cmd()) -e $script $example`)
-            error(
-                "project environment of example ",
-                basename(example),
-                " could not be instantiated",
-            )
-        end
-    end
-end
-# Run examples asynchronously
-processes = let literatejl = joinpath(@__DIR__, "literate.jl")
-    map(examples) do example
-        return run(
-            pipeline(
-                `$(Base.julia_cmd()) $literatejl $(basename(example)) $EXAMPLES_OUT`;
-                stdin=devnull,
-                stdout=devnull,
-                stderr=stderr,
-            );
-            wait=false,
-        )::Base.Process
-    end
-end
-
-# Check that all examples were run successfully
-isempty(processes) || success(processes) || error("some examples were not run successfully")
-
-### Build documentation
-using Documenter
+using JuliaGPsDocs
 
 using AbstractGPs
 # If any features of AbstractGPs depend on optional packages (e.g. via @require),
 # make sure to load them here in order to generate the full API documentation.
+
+JuliaGPsDocs.generate_examples(AbstractGPs)
+
+### Build documentation
+using Documenter
 
 # Doctest setup
 DocMeta.setdocmeta!(
@@ -64,8 +34,12 @@ makedocs(;
         "The Main APIs" => "api.md",
         "Concrete Features" => "concrete_features.md",
         "Examples" =>
-            map(filter!(filename -> endswith(filename, ".md"), readdir(EXAMPLES_OUT))) do x
-                return joinpath("examples", x)
+            map(
+                basename.(
+                    filter!(isdir, readdir(joinpath(@__DIR__, "src", "examples"); join=true)),
+                ),
+            ) do x
+                joinpath("examples", x, "index.md")
             end,
     ],
     #strict=true,
