b5f38abece80e18ecfc092655a06209c0852a559
st-- <st--@users.noreply.github.com>
2022-04-08 18:23:47 +0200

    clean up mean_function tests without reactivating AD (#315)
    * clean up mean_function tests without reactivating AD
    
    * unify x...=
    
    * rename

--- test/mean_function.jl
+++ test/mean_function.jl
@@ -1,47 +1,46 @@
 @testset "mean_functions" begin
+    rng = MersenneTwister(123456)
+    N, D = 5, 3
+    x1 = randn(rng, N)
+    xD_colvecs = ColVecs(randn(rng, D, N))
+    xD_rowvecs = RowVecs(randn(rng, N, D))
+
     @testset "ZeroMean" begin
-        P = 3
-        Q = 2
-        D = 4
-        # X = ColVecs(randn(rng, D, P))
-        x = randn(P)
-        x̄ = randn(P)
-        f = ZeroMean{Float64}()
+        m = ZeroMean{Float64}()
 
-        for x in [x]
-            @test AbstractGPs._map_meanfunction(f, x) == zeros(size(x))
-            # differentiable_mean_function_tests(f, randn(rng, P), x)
+        for x in [x1, xD_colvecs, xD_rowvecs]
+            @test AbstractGPs._map_meanfunction(m, x) == zeros(N)
+            #differentiable_mean_function_tests(m, randn(rng, N), x)
+
+            # Manually verify the ChainRule. Really, this should employ FiniteDifferences, but
+            # currently ChainRulesTestUtils isn't up to handling this, so this will have to do
+            # for now.
+            y, pb = rrule(AbstractGPs._map_meanfunction, m, x)
+            @test y == AbstractGPs._map_meanfunction(m, x)
+            Δmap, Δf, Δx = pb(randn(rng, N))
+            @test iszero(Δmap)
+            @test iszero(Δf)
+            @test iszero(Δx)
         end
-
-        # Manually verify the ChainRule. Really, this should employ FiniteDifferences, but
-        # currently ChainRulesTestUtils isn't up to handling this, so this will have to do
-        # for now.
-        y, pb = rrule(AbstractGPs._map_meanfunction, f, x)
-        @test y == AbstractGPs._map_meanfunction(f, x)
-        Δmap, Δf, Δx = pb(randn(P))
-        @test iszero(Δmap)
-        @test iszero(Δf)
-        @test iszero(Δx)
     end
+
     @testset "ConstMean" begin
-        rng, D, N = MersenneTwister(123456), 5, 3
-        # X = ColVecs(randn(rng, D, N))
-        x = randn(rng, N)
         c = randn(rng)
         m = ConstMean(c)
 
-        for x in [x]
+        for x in [x1, xD_colvecs, xD_rowvecs]
             @test AbstractGPs._map_meanfunction(m, x) == fill(c, N)
-            # differentiable_mean_function_tests(m, randn(rng, N), x)
+            #differentiable_mean_function_tests(m, randn(rng, N), x)
         end
     end
-    @testset "CustomMean" begin
-        rng, N, D = MersenneTwister(123456), 11, 2
-        x = randn(rng, N)
-        foo_mean = x -> sum(abs2, x)
-        f = CustomMean(foo_mean)
 
-        @test AbstractGPs._map_meanfunction(f, x) == map(foo_mean, x)
-        # differentiable_mean_function_tests(f, randn(rng, N), x)
+    @testset "CustomMean" begin
+        foo_mean = x -> sum(abs2, x)
+        m = CustomMean(foo_mean)
+
+        for x in [x1, xD_colvecs, xD_rowvecs]
+            @test AbstractGPs._map_meanfunction(m, x) == map(foo_mean, x)
+            #differentiable_mean_function_tests(m, randn(rng, N), x)
+        end
     end
 end
